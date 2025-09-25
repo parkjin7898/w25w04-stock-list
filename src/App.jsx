e9a9502597306ff220d9f5bc15d16de3328ee60b
parkjin7898 <parkjin7898@naver.com>
2025-09-25 17:42:52 +0900

    💄Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,25 @@ const stocks = [
 
 function App() {
   return (
-    <div>
-      <h1>오늘의 주식 시세</h1>
-      <div>
+    <div className='bg-gray-100 min-h-screen p-8 flex flex-col items-center'>
+      {/* bg-gray-100: 배경색을 연한 회색으로,
+      min-h-screen : 최소 높이를 화면 전체로 설정,
+      p-8: 패딩을 8로 설정,
+      flex flex-col: 플렉스 박스를 세로 방향으로 설정,
+      items-center: 아이템들을 가로축 중앙 정렬 */}
+      <h1 className="text-4xl font-bold text-gray-800 mb-10">오늘의 주식 시세</h1>
+      {/* text 4xl: 글자 크기를 4xl로,
+      font-bold: 글자를 굵게,
+      text-gray-800: 글자 색상을 진한 회색으로,
+      mb-10: 아래쪽 마진을 10으로 설정 */}
+      <div className="grid grid-cols-1 sm:grid-cols-2
+        lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full max-w-7xl">
+        {/* grid grid-cols-1: 그리드 레이아웃을 1열로 설정,
+        gap-6: 아이템들 사이의 간격을 6으로 설정 */}
+        {/* 반응형 웹사이트:
+        sm인 경우 2열, lg인 경우 3열, xl인 경우 4열로 설정,
+        w-full: 가로폭을 100%로 설정,
+        max-w-7xl: 최대 가로폭을 7xl로 설정 */}
         {
           stocks.map(stock => (
             <TickerCard 
--- src/TickerCard.jsx
+++ src/TickerCard.jsx
@@ -5,22 +5,50 @@ const TickerCard = ({ ticker, name, currentPrice, previousClose }) => {
   const isPositive = priceChange >= 0
 
   return (
-    <div>
-      <div> 
-        <h2>{name}</h2>
-        <div>{ticker}</div>
+    <div className="bg-white rounded-lg shadow-xl p-6 w-80
+    transform transition duration-500 hover:scale-105">
+      {/* bg-white: 배경색을 흰색으로,
+      rounded-lg: 모서리를 둥글게,
+      shadow-xl: 그림자 효과를 크게,
+      p-6: 패딩을 6으로 설정,
+      w-80: 가로폭을 80으로 설정,
+      transform transition duration-500 hover:scale-105:
+      hovering시에 500ms동안 scale하는데, 크기가 5% 커지는 애니메이션 효과 적용 */}
+      <div className="flex justify-between items-center mb-4"> 
+        {/* flex: 플렉스 박스 사용,
+        justify-between: 아이템들을 양 끝으로 정렬,
+        items-center: 아이템들을 세로축 중앙 정렬,
+          -> 이건 name과 ticker가 크기가 다를 때 유용
+        mb-4: 아래쪽 마진을 4로 설정 */}
+        <h2 className="text-2xl font-bold text-gray-800">{name}</h2>
+        <div className="text-sm font-semibold text-gray-500">{ticker}</div>
+        {/* 두 개 크기가 서로 다르지만
+        세로축 중앙 정렬되어서 보기 좋음 */}
       </div>
-      <div></div>
+      <div className="border-b border-gray-200 mb-4"></div>
+      {/* border-b: 아래쪽에만 테두리 적용,
+      border-gray-200: 테두리 색상을 연한 회색으로,
+      mb-4: 아래쪽 마진을 4로 설정 */}
       
-      <div>
+      <div className={`text-4xl font-extrabold mb-2
+                      ${isPositive ? 'text-green-600' : 'text-red-600'}`}>
+        {/* template literal (백틱) 사용
+        3항연산자를 써서 isPositive에 따라
+        글자 색상을 초록색 또는 빨간색으로 설정 */}
+        {/* templat literal이란? 백틱(`)으로 감싸서 문자열 안에 변수나 표현식을 삽입할 수 있는 방법
+        파이썬의 f-string과 비슷 */}
         ${currentPrice.toFixed(2)}
       </div>
 
-      <div>
+      <div className={`text-base font-semibold
+                      ${isPositive ? 'text-green-700' : 'text-red-700'}`}>
+        {/* template literal (백틱) 사용
+        3항연산자를 써서 isPositive에 따라
+        글자 색상을 초록색 또는 빨간색으로 설정 */}
         {isPositive ? '▲' : '▼'} {priceChange.toFixed(2)}
       </div>
       
-      <div>
+      <div className="mt-4 text-sm text-gray-500">
         전일 종가: ${previousClose.toFixed(2)}
       </div>
     </div>
